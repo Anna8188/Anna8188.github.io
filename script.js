416b50881ae18f744fa0de49b506ffbf9e2b87a3
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2022-02-07 02:52:05 +0400

    feat:add input, button, and box for information

---
+++ script.js
@@ -0,0 +1,7 @@
+let img = document.getElementsByClassName("img")[0];
+let name = document.getElementsByClassName("name-country")[0];
+let capital = document.getElementsByClassName("capital")[0];
+let population = document.getElementsByClassName("population")[0];
+let area = document.getElementsByClassName("area")[0];
+let language = document.getElementsByClassName("languages")[0];
+console.log(img, name, capital, population, area, language);
