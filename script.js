3f283bb9d11eab0fb650b12db8b6ecd9deb6e382
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2022-02-07 04:59:11 +0400

    fix: change background color, and some css parameters

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let img = document.getElementsByTagName("img")[0];
-let nameOfCountry = document.getElementsByClassName("name-country")[0];
-let capital = document.getElementsByClassName("capital")[0];
-let population = document.getElementsByClassName("population")[0];
-let area = document.getElementsByClassName("area")[0];
-let language = document.getElementsByClassName("languages")[0];
+const img = document.getElementsByTagName("img")[0];
+const button = document.getElementsByTagName("button")[0];
+const input = document.getElementsByTagName("input")[0];
+const nameOfCountry = document.getElementsByClassName("name-country")[0];
+const capital = document.getElementsByClassName("capital")[0];
+const population = document.getElementsByClassName("population")[0];
+const area = document.getElementsByClassName("area")[0];
+const language = document.getElementsByClassName("languages")[0];
 
-let searchCountry = function (nameOfCountry) {
-  fetch(`https://restcountries.com/v3.1/name/${nameOfCountry}`)
+button.addEventListener("click", function () {
+  fetch(`https://restcountries.com/v3.1/name/${input.value}`)
     .then((response) => {
       return response.json();
     })
@@ -16,12 +18,12 @@ let searchCountry = function (nameOfCountry) {
 
   function getData(data) {
     img.src = data.map((item) => item.flags.png);
-    console.log(data);
     nameOfCountry.innerHTML = data.map((item) => item.name.official);
-    capital.innerHTML += " " + data.map((item) => item.capital[0]);
-    population.innerHTML += " " + data.map((item) => item.population);
-    area.innerHTML += " " + data.map((item) => item.area);
-    language.innerHTML +=
-      " " + data.map((item) => Object.values(item.languages)[0]);
+    capital.innerHTML = "Capital | " + data.map((item) => item.capital[0]);
+    population.innerHTML =
+      "Population | " + data.map((item) => item.population);
+    area.innerHTML = "Area | " + data.map((item) => item.area);
+    language.innerHTML =
+      "Language | " + data.map((item) => Object.values(item.languages)[0]);
   }
-};
+});
